6370cba7e52012ffc364b4e222e4caa106a29ba2
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-12-30 21:29:57 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+!(function () {
+
+  // VARS (DOM)
+  const tips = document.getElementById("tips");
+  const imgs = document.getElementById("images");
+  const back = document.getElementById("back");
+  const play = document.getElementById("play");
+  const next = document.getElementById("next");
+  const close = document.getElementById("close");
+  let speed = 1;
+  // VARS
+  const letters = [];
+
+  // SPELL WORD
+  document.getElementById("txt").addEventListener("change", function () {
+
+    const txt = document.getElementById("txt"); // TEXT INPUT
+    let v = [...txt.value];
+    console.log(v);
+
+    // note, need to cleanup the filenames and take out the asl prefix below
+
+    for (let i = 0; i < v.length; i++) setTimeout(() => { console.log(i,v[i]);  document.getElementById("pic").src = `./assets/images/asl${v[i]}.gif` }, i * 1000 * speed);
+
+  });
+
+  // REVIEW
+  function review() {
+
+  }
+
+})();
